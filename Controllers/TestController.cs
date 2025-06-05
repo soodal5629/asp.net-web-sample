316b3a93cdfb1143f4f5af22d223b702a5ed4f1e
jcl <chaelynjang@lotte.net>
2025-06-05 16:53:29 +0900

    form, Controller 예제 확인

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -45,12 +45,14 @@ public class TestController : Controller
     }
 
     // IActionResult 은 View 인터페이스 구현체
-    public IActionResult testView() {
+    public IActionResult testView()
+    {
         //return View("~/Views/Home/test.cshtml"); 
         return View(); // 경로 생략도 가능 - 컨트롤러 이름에서 앞부분만 딴 폴더 이름 생성 ex) Views/Test/액션(메서드) 이름.cshtml
     }
 
-    public IActionResult testDataView() {
+    public IActionResult testDataView()
+    {
         // 1. ViewData 이용 - 딕셔너리 구조
         ViewData["myMsg"] = "Hello reponse";
         ViewData["num1"] = 100;
@@ -58,11 +60,24 @@ public class TestController : Controller
         // 2. ViewBag 이용 - 동적으로 속성 추가 가능
         ViewBag.MyTest = "hello myTest";
         ViewBag.num2 = 200;
-        ViewBag.arr  = new List<string>{"abc", "def"};
+        ViewBag.arr = new List<string> { "abc", "def" };
 
         // 3. ViewModel 이용 - return View 파라미터에 넘겨줘야 함
-        var numList = new List<int>{1, 2, 3};
-        return View();
-//        return View(numList);
+        var numList = new List<int> { 1, 2, 3 };
+
+        return View(numList);
+    }
+
+    public IActionResult testForm()
+    {
+        return View(); // 경로 생략도 가능 - 컨트롤러 이름에서 앞부분만 딴 폴더 이름 생성 ex) Views/Test/액션(메서드) 이름.cshtml
+    }
+    
+    [HttpPost]
+    public IActionResult testForm(string userId, int age)
+    {
+        Console.WriteLine("userId = " + userId);
+        Console.WriteLine("age = " + age);
+        return Redirect("/test/testView");
     }
 }
